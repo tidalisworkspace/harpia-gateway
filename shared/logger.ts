1e17319365da0b052b890e80aa12c0b282f2c123
Doug <douglasandrademiguel@gmail.com>
2022-08-26 05:24:18 -0300

    chore: add logger format

--- shared/logger.ts
+++ shared/logger.ts
@@ -1,3 +1,8 @@
 import logger from "electron-log";
 
+const format = "{d}/{m}/{y} {h}:{i}:{s} {level} {text}";
+
+logger.transports.console.format = format;
+logger.transports.file.format = format;
+
 export default logger;
