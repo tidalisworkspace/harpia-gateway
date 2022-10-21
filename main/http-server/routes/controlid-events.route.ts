806ead6cd4d012712fc72e79a2221936eccad167
Douglas Andrade <douglasandrademiguel@gmail.com>
2022-10-20 22:47:10 -0300

    fix: add controlid controller to controlid route

--- main/http-server/routes/controlid-events.route.ts
+++ main/http-server/routes/controlid-events.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import controller from "../controllers/hikvision-events.controller";
+import controller from "../controllers/controlid-events.controller";
 
 const router = express.Router();
 
