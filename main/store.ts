fdc8e0339311c37a524f8c2087e16f7e890f8a41
Douglas Andrade <douglasandrademiguel@gmail.com>
2022-12-04 19:54:36 -0300

    feat: improve store logic to save and get whitelist

--- main/store.ts
+++ main/store.ts
@@ -9,8 +9,24 @@ class Store extends ElectronStore {
     this.cypher = new Cypher();
   }
 
-  private normalize(value: string) {
+  private toKey(value: string) {
     return value.replaceAll(".", "");
+  }
+
+  private fromWhiteList(whiteList: string[]): string {
+    return whiteList
+      .filter(Boolean)
+      .map((item) => item.trim())
+      .filter(Boolean)
+      .join(",");
+  }
+
+  private toWhiteList(value: string): string[] {
+    return (value || "")
+      .split(",")
+      .filter(Boolean)
+      .map((item) => item.trim())
+      .filter(Boolean);
   }
 
   getSecret(key: string, defaultValue?: any): string {
@@ -28,29 +44,29 @@ class Store extends ElectronStore {
   }
 
   getHardwareConnection(ip: string) {
-    const key = this.normalize(ip);
+    const key = this.toKey(ip);
     return super.get(`hardware.${key}.connection`);
   }
 
   setHardwareConnection(ip: string, connection: string) {
-    const key = this.normalize(ip);
+    const key = this.toKey(ip);
     super.set(`hardware.${key}.connection`, connection);
   }
 
-  setWhiteList(ip: string, whitelist: string[]): void {
-    const key = this.normalize(ip);
-    super.set(`whitelist.${key}`, whitelist.join(","));
+  setWhiteList(ip: string, whiteList: string[]): void {
+    const key = this.toKey(ip);
+    super.set(`whitelist.${key}`, this.fromWhiteList(whiteList));
   }
 
   getWhiteList(ip: string): string[] {
-    const key = this.normalize(ip);
+    const key = this.toKey(ip);
     const value = super.get(`whitelist.${key}`) as string;
-    return value.split(",");
+    return this.toWhiteList(value);
   }
 
   deleteWhiteList(ip: string): void {
-    const key = this.normalize(ip);
-    super.set(`whitelist.${key}`, "");
+    const key = this.toKey(ip);
+    super.delete(`whitelist.${key}`);
   }
 }
 
