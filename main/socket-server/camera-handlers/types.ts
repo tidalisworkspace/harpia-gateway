33d9c36692622a8989333de8e883769eaa5dafda
Douglas Andrade <douglasandrademiguel@gmail.com>
2022-12-06 00:03:50 -0300

    feat: add alphadigi camera types

---
+++ main/socket-server/camera-handlers/types.ts
@@ -0,0 +1,50 @@
+export enum MessageType {
+  HEARTBEAT = 1,
+  NOTIFICATION = 2,
+  REQUEST = 3,
+  RESPONSE = 4,
+}
+
+export interface MessageHeader {
+  magic: string;
+  version: number;
+  msgtype: number;
+  datatype: number;
+  resv: string;
+  timestamp: number;
+  seq: number;
+  datasize: number;
+}
+
+export enum HeaderValueType {
+  NUMBER = "number",
+  TEXT = "text",
+}
+
+export interface HeaderValueDefinition {
+  name: string;
+  size: number;
+  start: number;
+  end: number;
+  type: HeaderValueType;
+}
+
+export interface HeaderValueWritter {
+  type: HeaderValueType;
+  write: (header: MessageHeader, definition: HeaderValueDefinition) => Buffer;
+}
+
+export interface HeaderValueReader {
+  type: HeaderValueType;
+  read: (data: Buffer, definition: HeaderValueDefinition) => number | string;
+}
+
+export const headerMagic = "QYZN";
+
+export const headerSize = 20;
+
+export interface SocketCameraConnectionHandler {
+  readonly name: string;
+  readonly messageType: MessageType;
+  handle(connectionId: string, header: MessageHeader): Promise<void>;
+}
