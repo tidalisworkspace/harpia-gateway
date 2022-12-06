e5b951b6fe9605e0b9b5630232227b4e1e3190f4
Douglas Andrade <douglasandrademiguel@gmail.com>
2022-12-06 01:26:40 -0300

    feat: add alphadigi heartbeat simple handler

---
+++ main/socket-server/camera-handlers/heartbeat.handler.ts
@@ -0,0 +1,20 @@
+import { HeaderWritter } from "./header-writter";
+import {
+  MessageHeader,
+  MessageType,
+  SocketCameraConnectionHandler,
+} from "./types";
+
+export class HeartbeatHandler implements SocketCameraConnectionHandler {
+  name = "heartbeat";
+  messageType = MessageType.HEARTBEAT;
+  headerWritter: HeaderWritter;
+
+  constructor(headerWritter: HeaderWritter) {
+    this.headerWritter = headerWritter;
+  }
+
+  async handle(connectionId: string, header: MessageHeader): Promise<void> {
+    throw new Error("heartbeat not implemented");
+  }
+}
