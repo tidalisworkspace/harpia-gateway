381427a3bff13b437cb2a92149d0f3f7ad3eda8f
Douglas Andrade <douglasandrademiguel@gmail.com>
2022-12-12 02:16:01 -0300

    feat: remove log when erro on json parse

--- main/socket-server/camera-handlers/body-reader.ts
+++ main/socket-server/camera-handlers/body-reader.ts
@@ -4,9 +4,10 @@ import { headerSize } from "./types";
 export class BodyReader {
   read(data: Buffer): any {
     try {
-      return JSON.parse(data.subarray(headerSize).toString());
+      const body = JSON.parse(data.subarray(headerSize).toString());
+      logger.error(`body-reader:read ${JSON.stringify(body)}`);
+      return body;
     } catch (e) {
-      logger.error(`body-reader:read ${e.name}:${e.message}`);
       return null;
     }
   }
